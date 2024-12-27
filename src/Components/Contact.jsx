0d2cafc1516cb0bf46a806ff98b64419d6add089
fevos4 <fevatesfish@gmail.com>
2024-12-27 21:38:21 +0300

    updated

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -51,26 +51,43 @@ const Contact = () => {
         </div>
 
         {/* Right Section: Contact Info */}
-        <div className="md:w-1/2">
-          <h3 className="text-xl font-bold text-black mb-4 font-montserrat">
-            {t('contactUsHeader')}
-          </h3>
-          <div className="space-y-4">
-            {/* Phone */}
-            <div className="flex items-center">
-              <FaPhone className="text-lg text-gray-600 mr-3" />
-              <p className="text-gray-700 text-lg font-montserrat">
-                {t('contactDetails.phone')}
-              </p>
-            </div>
+        <div className="md:w-1/2 space-y-6">
+          <div>
+            <h3 className="text-xl font-bold text-black mb-4 font-montserrat">
+              {t('contactUsHeader')}
+            </h3>
+            <div className="space-y-4">
+              {/* Phone */}
+              <div className="flex items-center">
+                <FaPhone className="text-lg text-gray-600 mr-3" />
+                <p className="text-gray-700 text-lg font-montserrat">
+                  {t('contactDetails.phone')}
+                </p>
+              </div>
 
-            {/* Address */}
-            <div className="flex items-center">
-              <FaMapMarkerAlt className="text-lg text-gray-600 mr-3" />
-              <p className="text-gray-700 text-lg font-montserrat">
-                {t('contactDetails.address')}
-              </p>
+              {/* Address */}
+              <div className="flex items-center">
+                <FaMapMarkerAlt className="text-lg text-gray-600 mr-3" />
+                <p className="text-gray-700 text-lg font-montserrat">
+                  {t('contactDetails.address')}
+                </p>
+              </div>
             </div>
+          </div>
+
+          {/* Google Map */}
+          <div className="mt-6">
+          <iframe
+ src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3293.052008639912!2d39.26570127406203!3d8.548058196333791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b1f0015b2da1b%3A0x9d086afc87d5ba02!2sAlpha%20girls%20gym!5e1!3m2!1sen!2set!4v1735324234384!5m2!1sen!2set"
+  width="100%"
+  height="300"
+  style={{ border: 0 }}
+  allowFullScreen=""
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+  title="Google Map"
+></iframe>
+
           </div>
         </div>
       </div>
